a73db4775cf81dff90a8edf172a67aa1a9c59304
Shannon Pileggi <pileggis@mskcc.org>
2022-04-10 07:30:28 -0400

    update how to sign up

--- R/helpers.R
+++ R/helpers.R
@@ -14,7 +14,12 @@ compose_email <- function(data) {
     glue("\n\n") %>% 
     glue("We would be very happy to have you serve as a curator! If you would like more information prior to making a decision, please read the Rotating Curator Guide (https://guide.rladies.org/rocur/) and experiences from previous curators (https://guide.rladies.org/rocur/guide/#work-from-previous-curators--the-wearerladies-team).\n\n") %>% 
     glue("\n\n") %>% 
-    glue("If you are ready to sign up please review our current schedule of curators (https://tinyurl.com/wearerladies-schedule) and submit the sign up form (https://tinyurl.com/wearerladies-sign-up), which takes approximately 20 minutes to complete.\n\n") %>% 
+    glue("If you are ready to sign up, here is the process:") %>% 
+    glue("1. Please review our current schedule of curators (https://tinyurl.com/wearerladies-schedule) and identify a first and second choice open week you would like to curate.\n\n") %>% 
+    glue("2. Submit the sign up form (https://tinyurl.com/wearerladies-sign-up), which takes approximately 20 minutes to complete. You submit your desired weeks in this form.\n\n") %>%
+    glue("3. Email WeAre@rladies.org with a photo of yourself for the curator graphic.\n\n") %>% 
+    glue("4. Once we have your sign up form and your photo, a member of the RoCur team will email you to confirm your scheduled week and provide you more information about curating.\n\n") %>% 
+    glue("\n\n") %>% 
     glue("\n\n") %>% 
     glue("If you are interested in curating, but not immediately, please consider this an open invitation to schedule yourself when your time allows.\n\n") %>% 
     glue("\n\n") %>% 
